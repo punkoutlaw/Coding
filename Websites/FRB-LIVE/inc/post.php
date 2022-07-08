896f56f3e01d616bb66f58f9d5bd6815752860b9
David Feudale <dwfeudale@gmail.com>
2022-07-08 11:03:05 -0400

    updated beta contact files

--- Websites/FRB-LIVE/inc/post.php
+++ Websites/FRB-LIVE/inc/post.php
@@ -1,14 +1,14 @@
 <?php
 
 // check the honeypot
-$honeypot = filter_input(INPUT_POST, 'nickname', FILTER_SANITIZE_SPECIAL_CHARS);
+$honeypot = filter_input(INPUT_POST, 'nickname', FILTER_UNSAFE_RAW);
 if ($honeypot) {
     header($_SERVER['SERVER_PROTOCOL'] . ' 405 Method Not Allowed');
     exit;
 }
 
 // validate name
-$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+$name = filter_input(INPUT_POST, 'name', FILTER_UNSAFE_RAW);
 $inputs['name'] = $name;
 if (!$name || trim($name) === '') {
     $errors['name'] = 'Please enter your name';
@@ -27,14 +27,14 @@ if ($email) {
 }
 
 // validate subject
-$subject = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_SPECIAL_CHARS);
+$subject = filter_input(INPUT_POST, 'subject', FILTER_UNSAFE_RAW);
 $inputs['subject'] = $subject;
 if (!$subject || trim($subject) === '') {
     $errors['subject'] = 'Please enter the subject';
 }
 
 // validate message
-$message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_SPECIAL_CHARS);
+$message = filter_input(INPUT_POST, 'message', FILTER_UNSAFE_RAW);
 $inputs['message'] = $message;
 if (!$message || trim($message) === '') {
     $errors['message'] = 'Please enter the message';
