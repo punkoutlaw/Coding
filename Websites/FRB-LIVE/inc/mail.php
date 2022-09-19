bbf4088507483d9b621f7b50d33baa9e9479c983
David Feudale <dwfeudale@gmail.com>
2022-09-19 15:31:46 -0400

    updated pics

--- Websites/FRB-LIVE/inc/mail.php
+++ Websites/FRB-LIVE/inc/mail.php
@@ -7,8 +7,8 @@ $recipient_email = $config['mail']['to_email'];
 // contact information
 $contact_name = $inputs['name'];
 $contact_email = $inputs['email'];
-$message = $inputs['message'];
 $subject = $inputs['subject'];
+$message = $inputs['message'];
 
 // Email header
 $headers[] = 'MIME-Version: 1.0';
@@ -17,5 +17,5 @@ $headers[] = "To: $recipient_email";
 $headers[] = "From: $contact_email";
 $header = implode('\r\n', $headers);
 
-$body = "\nMessage: " . $message . "\nName: " . $contact_name . "\nEmail Address: " . $contact_email;
+$body = "\nName: " . $contact_name . "\nEmail Address: " . $contact_email . "\nMessage: " . $message;
 mail($recipient_email, $subject, $body, $header);
