e12e689319e53b7aacd55f784e2067425c16ee64
David Feudale <98500025+punkoutlaw@users.noreply.github.com>
2023-01-15 21:43:56 -0500

    updated/added school files

---
+++ Projects/UFSWDB/JavaScript101/script.js
@@ -0,0 +1,19 @@
+console.log("Page successfully loaded script.js");
+
+var website = "https://codingforeverybody.com";
+var teacherName = "Kalob Taulien";
+
+console.log(website)
+console.log(teacherName);
+
+let apples = 1;
+const height = "5'10\""
+
+// COMMENTS \\
+
+/*
+"=" is the assignment operator
+var can be used within or outside its scope (in or outside {} brackets)
+let can only be used within its scope
+const is a variable that cannot be re-declared
+*/
