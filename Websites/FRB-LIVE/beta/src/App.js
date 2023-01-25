6a60b425f22fe6fc4fe560f934f5d45362d31ef3
David Feudale <98500025+punkoutlaw@users.noreply.github.com>
2023-01-25 16:12:02 -0500

    added accordion/styling

--- Websites/FRB-LIVE/beta/src/App.js
+++ Websites/FRB-LIVE/beta/src/App.js
@@ -4,12 +4,14 @@ import React from 'react';
 import Nav from './components/nav';
 import Main from './components/main';
 import Footer from './components/footer';
+import Mailing from './components/mailing';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Main />
+      <Mailing />
       <Footer />
     </div>
   );
---
+++ Websites/FRB-LIVE/beta/src/components/mailing.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+
+const Mailing = () => {
+  return (
+    <div id="subscribe" className="main">
+
+
+    <Accordion defaultActiveKey="0" flush>
+      <Accordion.Item eventKey="0">
+        
+        <Accordion.Header> Subscribe to our mailing list! </Accordion.Header>
+
+          <Accordion.Body>
+          
+            <form 
+              action="forms/subscribe.php" 
+              onsubmit="alert('Thank you for subscribing!')">
+
+              <div className="col mx-auto">
+                <input type="text" className="form-control" placeholder="Name" name="name" required />
+                
+                <br></br>
+
+                <input type="text" className="form-control" placeholder="Email Address" name="email" required />
+
+                <br></br>
+                <br></br>
+
+                <label>
+                  <input type="checkbox" checked="checked" name="subscribe" /> Newsletter
+                </label>
+                <input type="submit" value="Subscribe" className="sub-button bg-primary" />
+              </div>
+
+            </form>
+          
+          </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+
+  </div>
+  
+  )
+}
+
+export default Mailing
