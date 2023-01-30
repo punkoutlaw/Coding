56d8c6f5c868a22c4c4f9df5089d164e02299c8a
David Feudale <98500025+punkoutlaw@users.noreply.github.com>
2023-01-30 16:09:52 -0500

    added contact page

--- Websites/FRB-LIVE/beta/src/pages/contact.js
+++ Websites/FRB-LIVE/beta/src/pages/contact.js
@@ -1,7 +1,92 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
 
 const Contact = () => {
-    return <h1>Contact</h1>;
+    return (
+
+      <div id="contact" className="container">
+
+      <Card className="div3">
+
+      <Card.Header className="headers">
+        <h2>Contact</h2>
+      </Card.Header>
+      
+      <Card.Body>
+        <div className="row">
+  
+          <div className="col-6 contact-list" id="contact-icons">
+  
+            <br/>
+  
+            <ul className="li-form">
+              <li><i className="bi bi-envelope-fill"></i> info@feudalerockband.com </li>
+              <li><i className="bi bi-question-circle"></i> Send inquiries below <i className="bi bi-arrow-down"></i></li>
+            </ul>
+          </div>
+  
+          <div className="col-6 contact-photo">
+            <br/>
+            <img src="./Images/FEUDALE_contact.jpg" alt="BAND PHOTO" id="contact-photo"/>
+            
+          </div>
+  
+        </div>
+  
+        <div className="row">
+          <div className="col">
+                    
+            <form action="forms/contact.php"
+              method="post"
+              name="EmailForm"
+              id="frb-form"
+              onsubmit="alert('Thank you for contacting us! We will be in touch shortly.')">
+  
+                <div className="input-text" id="input-headers">
+                    
+                    <label for="name">Full Name</label>
+                    <br/>
+                    <input type="text" name="name" className="form-control" 
+                      placeholder="Full name" required="required" />
+                    <label for="email">Email Address</label>
+                    <br/>
+                    <input type="text" name="email" className="form-control" 
+                      placeholder="Email address" required="required" />
+            
+                    <label for="subject">Subject</label>
+                    <br/>
+                    <input type="text" name="subject" className="form-control" 
+                      placeholder="Enter a subject" required="required" />
+          
+                    <label for="message">Message</label>
+                    <br/>
+                    <textarea className="form-control" name="message" 
+                      rows="4" placeholder="Your message" required="required"></textarea>
+                    <br/>
+  
+                    <div className="g-recaptcha" data-sitekey="6LfRQywhAAAAADg-4BZVR4ROA0FDFkdMGv8XGhdM"></div>
+  
+                    <br/>
+  
+                    <button type="submit" 
+                      value="submit"
+                      className="btn btn-primary">
+                      Submit
+                    </button>
+  
+                </div>
+  
+            </form>
+  
+          </div>      
+        </div>
+    
+      </Card.Body>
+    </Card>
+
+    </div>
+
+    )
   };
   
   export default Contact;
--- Websites/FRB-LIVE/beta/src/pages/videos.js
+++ Websites/FRB-LIVE/beta/src/pages/videos.js
@@ -1,7 +1,55 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+import Iframe from 'react-iframe';
 
 const Videos = () => {
-    return <h1>Videos</h1>;
+    return (
+
+      <div id="videos" className="container">
+
+      <Card className="div3">
+
+      <Card.Header className="headers">
+        <h2>Videos</h2>
+      </Card.Header>
+      
+        <Card.Body>
+
+          <div className="row">
+            <div className="col">
+              <label><b>It's Alright</b>
+                <br/>
+                  <Iframe className="iframes" src="https://youtube.com/embed/XxONBMf0GMI" title="It's Alright">
+                  </Iframe>
+                </label>
+                <label><b>Teenager</b>
+                <br/>
+                  <Iframe className="iframes" src="https://youtube.com/embed/_zWJcNywMNg" title="Teenager">
+                  </Iframe>
+                </label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <label><b>It's Alright (LIVE)</b>
+              <br/>
+                <Iframe className="iframes" src="https://youtube.com/embed/u9iv1lul4Lo" title="It's Alright (LIVE)">
+                </Iframe>
+              </label>
+              <label><b>Wicked Garen (STP Cover)</b>
+              <br/> 
+                <Iframe className="iframes" src="https://youtube.com/embed/vsWYeXiJVMQ" title="Wicked Garden">
+                </Iframe>
+              </label>
+            </div>
+          </div>
+        </Card.Body>
+
+      </Card>
+
+      </div>
+
+    )
   };
   
   export default Videos;
