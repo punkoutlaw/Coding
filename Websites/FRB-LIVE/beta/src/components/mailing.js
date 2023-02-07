273a62024c8b1cd291a473b6c9ad99d4590cfb6a
David Feudale <98500025+punkoutlaw@users.noreply.github.com>
2023-02-07 18:41:38 -0500

    updated mailing list

--- Websites/FRB-LIVE/beta/src/components/mailing.js
+++ Websites/FRB-LIVE/beta/src/components/mailing.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import emailjs from '@emailjs/browser';
 import Accordion from 'react-bootstrap/Accordion';
 
+const Result = () => {
+  return(
+    <p>You have successfully subscribed!</p>
+  );
+};
+
 const Mailing = () => {
+
+  const [result, showResult] = useState(false);
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_gbtxu0e', 'template_obmd2fm', e.target, '0D8ZXRHZo14Tuc3Xv')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+      showResult(true);
+  };
+
   return (
     <div id="subscribe" className="main">
 
@@ -13,12 +35,10 @@ const Mailing = () => {
 
           <Accordion.Body>
           
-            <form 
-              action="/subscribe.php" 
-              onSubmit="alert('Thank you for subscribing!')">
+            <form onSubmit={sendEmail}>
 
               <div className="col mx-auto">
-                <input type="text" className="form-control" placeholder="Name" name="name" required />
+                <input type="text" className="form-control" placeholder="Name" name="fullName" required />
                 
                 <br></br>
 
@@ -28,9 +48,17 @@ const Mailing = () => {
                 <br></br>
 
                 <label>
-                  <input type="checkbox" checked="checked" name="subscribe" /> Newsletter
+                  <input type="checkbox" name="subscribe" value="Yes"/> Newsletter
                 </label>
                 <input type="submit" value="Subscribe" className="sub-button bg-primary" />
+
+                <br />
+                <br />
+
+                <div className="row">
+                  {result ? <Result /> : null} 
+                </div>
+
               </div>
 
             </form>
