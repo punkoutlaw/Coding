75350e4a9a68f1d2265dde85bdf4dea89faa350e
David Feudale <98500025+punkoutlaw@users.noreply.github.com>
2023-02-07 17:00:36 -0500

    updated form

--- Websites/FRB-LIVE/beta/src/components/form.js
+++ Websites/FRB-LIVE/beta/src/components/form.js
@@ -1,54 +1,65 @@
-import React from 'react';
-import ReCAPTCHA from 'react-google-recaptcha';
+import React, { useState } from 'react';
 import emailjs from '@emailjs/browser';
 
-const Form = () => {
+const Result = () => {
+  return(
+    <p>Your message has been sent!</p>
+  );
+};
 
+const Form = (props) => {
+
+  const [result, showResult] = useState(false);
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_gbtxu0e', 'template_tkkjhda', Form.current, '0D8ZXRHZo14Tuc3Xv')
+    emailjs.sendForm('service_gbtxu0e', 'template_tkkjhda', e.target, '0D8ZXRHZo14Tuc3Xv')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
+      e.target.reset();
+      showResult(true);
   };
   
   return (
-    
-    <form ref={Form} onSubmit={sendEmail} id="frb-form">
+    <form onSubmit={sendEmail} id="frb-form">
 
-      <div className="input-text" id="input-headers">
-          
-          <label for="name">Full Name</label>
-          <br/>
-          <input type="text" name="fullName" className="form-control" 
-            placeholder="Full name" required="required" />
-          <label for="email">Email Address</label>
-          <br/>
-          <input type="text" name="email" className="form-control" 
-            placeholder="Email address" required="required" />
-  
-          <label for="subject">Subject</label>
-          <br/>
-          <input type="text" name="subject" className="form-control" 
-            placeholder="Enter a subject" required="required" />
+    <div className="input-text" id="input-headers">
+        
+        <label for="name">Full Name</label>
+        <br/>
+        <input type="text" name="fullName" className="form-control" 
+          placeholder="Full name" required="required" />
+        <label for="email">Email Address</label>
+        <br/>
+        <input type="text" name="email" className="form-control" 
+          placeholder="Email address" required="required" />
 
-          <label for="message">Message</label>
-          <br/>
-          <textarea className="form-control" name="message" 
-            rows="4" placeholder="Your message" required="required"></textarea>
+        <label for="subject">Subject</label>
+        <br/>
+        <input type="text" name="subject" className="form-control" 
+          placeholder="Enter a subject" required="required" />
 
-          <br/>
-          <input type="submit" value="Send" className='btn btn-primary'/>
-          <br/>
+        <label for="message">Message</label>
+        <br/>
+        <textarea className="form-control" name="message" 
+          rows="4" placeholder="Your message" required="required"></textarea>
 
-      </div>
+        <br/>
+        <input type="submit" value="Send" className='btn btn-primary'/>
+        <br/>
+
+        <div className="row">
+          {result ? <Result /> : null} 
+        </div>
+
+    </div>
 
     </form>
+  );
 
-  )
 }
 
 export default Form
