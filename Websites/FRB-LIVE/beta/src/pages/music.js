433a9945cb1b912e956567ace506102fb9fb829d
David Feudale <98500025+punkoutlaw@users.noreply.github.com>
2023-02-18 00:48:08 -0500

    updated music page/styling

--- Websites/FRB-LIVE/beta/src/pages/music.js
+++ Websites/FRB-LIVE/beta/src/pages/music.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import { Card, CardGroup, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Card, Collapse, Row, Col, Button } from 'react-bootstrap';
 import Iframe from 'react-iframe';
 
+
 const Music = () => {
+  const [open, setOpen] = useState(false);
+  const [open2, setOpen2] = useState(false);
+
   return (
 
     <div id="music" className="container">
@@ -15,41 +19,67 @@ const Music = () => {
     
     <Card.Body>
 
-      <br/>
-
-      {/* <div className='row'>
+      <div className='row'>
         <div className='col'>
-          <img src='./Images/itsalright-artwork.png' alt='Its Alright Album Art'/>
+          <div className='header'><h3>Singles <i class="bi bi-music-note"></i></h3></div>
         </div>
-        <div className='col'>
-          <img src='./Images/teenager-artwork.png' alt='Its Alright Album Art'/>
-        </div>
-      </div> */}
+      </div>
 
       <Row>
         <Col>
           <Card className='albumcard'>
-            <Card.Title className='albumtitle'><h3>It's Alright</h3></Card.Title>
             <Card.Img variant="top" className='albumart' src="./Images/itsalright-artwork.png" style={{width:"300px", height:"300px"}}/>
-            <Card.Body>
-              <Card.Text>
-                <p>It's Alright performed by FEUDALE</p>
-              </Card.Text>
-            </Card.Body>
+              <br/>
+                <Button
+                  onClick={() => setOpen(!open)}
+                  aria-controls="example-collapse-text"
+                  aria-expanded={open}
+                  className='album-buttons'
+                        >
+                  <label className='labeltitle'>It's Alright <i className='bi bi-info-circle'></i></label>
+                </Button>
+                <div style={{ minHeight: '150px' }} className='albumcollapse'>
+                  <Collapse in={open} dimension="width">
+                    <div id="example-collapse-text">
+                      <Card body style={{ width: '500px' }} className="album-collapse-text">
+                      "In these trying times "It's Alright " is a rock & roll anthem of the era! Through all of the ups and down, we wanted to send a message to the world that whatever life is throwing your way -  in the end - IT'S ALRIGHT!" -FEUDALE
+                      </Card>
+                    </div>
+                  </Collapse>
+                </div>
+
             <Card.Footer className='albumfooter'>
               <small className="text-muted">Released: 10/16/2020</small>
             </Card.Footer>
           </Card>
         </Col>
+        
         <Col>
           <Card className='albumcard'>
-          <Card.Title className='albumtitle'><h3>Teenager</h3></Card.Title>
             <Card.Img variant="top" className='albumart' src="./Images/teenager-artwork.png" style={{width:"300px", height:"300px"}}/>
-            <Card.Body>
-              <Card.Text>
-                <p>Teenager performed by FEUDALE</p>
-              </Card.Text>
-            </Card.Body>
+              <br/>
+                <Button
+                  onClick={() => setOpen2(!open2)}
+                  aria-controls="example-collapse-text"
+                  aria-expanded={open}
+                  className='album-buttons'
+                        >
+                  <label className='labeltitle'>Teenager <i className='bi bi-info-circle'></i></label>
+                </Button>
+                <div style={{ minHeight: '150px' }} className='albumcollapse'>
+                  <Collapse in={open2} dimension="width">
+                    <div id="example-collapse-text">
+                      <Card body style={{ width: '500px' }} className="album-collapse-text">
+                        <p>
+                        "We wrote “Teenager” based on our own experiences living and growing up in New Jersey. There was always something charming about hanging out down the shore and shopping (maybe getting kicked out) of the Freehold Raceway Mall. Or vibing in our bedroom listening to Pink Floyd and Nirvana.
+                        <br/><br/>
+                        When writing “Teenager”, we really wanted to encompass what it was like for us being teenagers. Our goal was to give our listeners a glimpse into our lives before becoming FEUDALE. It’s a song that will hopefully bring our listeners to a place in time that they will remember as magical, uncertain. And a ton of fun!” –FEUDALE
+                        </p>
+                      </Card>
+                    </div>
+                  </Collapse>
+                </div>
+
             <Card.Footer className='albumfooter'>
               <small className="text-muted">Released: 11/29/2019</small>
             </Card.Footer>
@@ -61,7 +91,7 @@ const Music = () => {
 
       <div className='row'>
         <Card.Title>
-          Now Playing on Spotify <i class="bi bi-spotify" id="spotify"></i>
+          <h3>Playing on Spotify <i class="bi bi-spotify" id="spotify"></i></h3>
         </Card.Title>
 
         <br/>
