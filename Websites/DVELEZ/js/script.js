bcca8daef6870ac06c3938b373d621ef0a1c3dce
David Feudale <98500025+punkoutlaw@users.noreply.github.com>
2023-04-24 11:47:58 -0400

    updated dvelez

---
+++ Websites/DVELEZ/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // $(".form-group").addClass("animate__animated animate__bounce");
+    $("#main").addClass("animate__animated animate__fadeIn");
+    $("#sub-contact").addClass("animate__animated animate__bounce");
+    $(".btn").addClass("animate__animated animate__fadeIn");
+});
