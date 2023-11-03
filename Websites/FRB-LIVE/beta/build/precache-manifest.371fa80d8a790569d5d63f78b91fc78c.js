ef9b090ff5cebba6aa4f4192b0b2f442b8ea6e47
David Feudale <dwfeudale@gmail.com>
2023-11-03 14:46:25 -0400

    modified formatting

---
+++ Websites/FRB-LIVE/beta/build/precache-manifest.371fa80d8a790569d5d63f78b91fc78c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ed3e8b40f2ea3c471d7",
+    "url": "/static/css/main.490042e6.chunk.css"
+  },
+  {
+    "revision": "8ed3e8b40f2ea3c471d7",
+    "url": "/static/js/main.8ed3e8b4.chunk.js"
+  },
+  {
+    "revision": "72de945a7fa5317e65ff",
+    "url": "/static/js/1.72de945a.chunk.js"
+  },
+  {
+    "revision": "a596a1fd4a7e6d480bdd",
+    "url": "/static/css/2.ce7404be.chunk.css"
+  },
+  {
+    "revision": "a596a1fd4a7e6d480bdd",
+    "url": "/static/js/2.a596a1fd.chunk.js"
+  },
+  {
+    "revision": "1003782132d2bd7c61cd",
+    "url": "/static/js/runtime~main.10037821.js"
+  },
+  {
+    "revision": "4674c4c7e36a56b03e199f31146576c5",
+    "url": "/index.html"
+  }
+];
---
+++ Websites/FRB-LIVE/beta/src/components/instafeed.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InstagramFeed from 'react-ig-feed';
+import 'react-ig-feed/dist/index.css';
+
+const Instafeed = () => {
+  return (
+    <InstagramFeed token="IGQVJVUGp2UUd5MUE3OWpObVh1QlQ2ZAms4NU4weFdQX2RqZA2h4YTNZANmlWcUdNdlAyNkdqdGJrNjV1Q2hTcWtGSFNCZAWZAWSlVBUFdkOUhyNlZAZARkpBN00tUmVHbkJjT0ItejJURWk0OVFxeE5ERUNyVQZDZD"
+    counter="6"/>
+  )
+}
+
+export default Instafeed
