b0383ed26e25f14675d4a14d430868c1e91c00cd
David Feudale <dwfeudale@gmail.com>
2023-11-15 13:05:08 -0500

    updated formatting

--- Websites/JCBARBER/assets/js/owl-carousel.js
+++ Websites/JCBARBER/assets/js/owl-carousel.js
@@ -3445,4 +3445,38 @@
 		$.support.transform3d = tests.csstransforms3d();
 	}
 
+	(function () {
+		"use strict";
+	  
+		var carousels = function () {
+		  $(".owl-carousel1").owlCarousel({
+			loop: true,
+			center: true,
+			margin: 0,
+			responsiveClass: true,
+			nav: false,
+			responsive: {
+			  0: {
+				items: 1,
+				nav: false
+			  },
+			  680: {
+				items: 2,
+				nav: false,
+				loop: false
+			  },
+			  1000: {
+				items: 3,
+				nav: true
+			  }
+			}
+		  });
+		};
+	  
+		(function ($) {
+		  carousels();
+		})(jQuery);
+	  })();
+	  
+
 })(window.Zepto || window.jQuery, window, document);
